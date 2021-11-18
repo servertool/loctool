cd1efea55b0c49e2a5ac62755daf88f01932c9be
servertool <94093751+servertool@users.noreply.github.com>
2021-11-18 07:25:07 -0700

    Create loc-tool.php

---
+++ loc-tool.php
@@ -0,0 +1,218 @@
+
+
+//Phần Cài Đặt Màu
+$ngay=date("d/m");
+$day=date("d/m");
+$ngay = date("d");
+$thang = date("m");
+$nam = date("Y");
+$loaitool="Tool Vip Nhiều Chế Độ";
+$vienden="\033[100m"; 
+$viendo="\033[101m"; 
+$vienluc="\033[102m"; 
+$vienvang="\033[103m"; 
+$vienxduong="\033[104m"; 
+$vienhong="\033[105m"; 
+$vienxnhac="\033[106m"; 
+$vientrang="\033[107m";
+$maufulltrang= "\e1;47;33m"; 
+$maufullxduong= "\e[1;47;34m"; 
+$maufulldo= "\e[1;47;31m"; 
+$maufullluc= "\e[1;47;32m"; 
+$maufullxnhac= "\e[1;47;36m"; 
+$maufullden= "\e[1;47;30m"; 
+$maufullvang= "\e[1;47;33m"; 
+$maufullhong= "\e[1;47;33m"; 
+$denthui="\033[0;30m"; 
+$res="\033[0m"; 
+$do="\033[0;31m"; 
+$grean="\033[1;32m"; 
+$mit="\033[0;32m"; 
+$vang="\033[0;33m"; 
+$mti="\033[0;33m"; 
+$test="\033[1;33m"; 
+$tim="\033[1;36m"; 
+$lam="\033[1;34m"; 
+$tmi="\033[1;34m"; 
+$hong="\033[1;35m"; 
+$imt="\033[1;35m"; 
+$blue="\033[1;36m"; 
+$trang="\033[1;37m"; 
+$ress="\033[0;32m"; 
+$res="\033[0;33m"; 
+$green="\033[0;37m"; 
+$yellow="\033[0;33m"; 
+$white= "\033[0;33m"; 
+$xnhac= "\033[1;96m"; 
+$den="\033[1;90m"; 
+$do="\033[1;91m"; 
+$luc="\033[1;92m"; 
+$vang="\033[1;93m"; 
+$xduong="\033[1;94m"; 
+$hong="\033[1;95m"; 
+$trang="\033[1;97m"; 
+$banner="\r\n"; 
+$ngay=date("d/m"); 
+$day=date("d/m"); 
+$BBlack="\033[1;30m"; 
+$BRed="\033[1;31m"; 
+$BGreen="\033[1;32m"; 
+$BYellow="\033[1;33m"; 
+$BBlue="\033[1;34m"; 
+$BPurple="\033[1;35m";
+$BPurpleb="\033[1;50m";  
+$BCyan="\033[1;36m"; 
+$BWhite="\033[1;37m"; 
+$Blue="\033[0;34m"; 
+$res="\033[0m"; 
+$red="\033[1;31m"; 
+$xanh="\033[1;32m"; 
+$cyan="\033[1;36m"; 
+$yellow="\033[1;33m"; 
+$turquoise="\033[1;34m";
+$maugi="\033[1;35m"; 
+$white= "\033[1;37m";
+$ngay = date("d"); 
+$thang = date("m"); 
+$nam = date("Y");
+$a=date('d'); // 
+	$b=date("m");
+	$thoigian=date('d/m');
+$day = [];
+$day= date('d-m-y');
+$today = date("d-m-y");
+$d = date("d-m");
+//Logo && Banner
+$bannertoan = "
+\033[1;37m  ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+\033[1;36m  ┃        ██╗      █████╗  █████╗       ████████╗ █████╗  █████╗ ██╗           ┃ 
+\033[1;37m  ┃        ██║     ██╔══██╗██╔══██╗      ╚══██╔══╝██╔══██╗██╔══██╗██║           ┃    
+\033[1;36m  ┃        ██║     ██║  ██║██║  ╚═╝█████╗   ██║   ██║  ██║██║  ██║██║           ┃ 
+\033[1;37m  ┃        ██║     ██║  ██║██║  ██╗╚════╝   ██║   ██║  ██║██║  ██║██║           ┃ 
+\033[1;36m  ┃        ███████╗╚█████╔╝╚█████╔╝         ██║   ╚█████╔╝╚█████╔╝███████╗      ┃ 
+\033[1;37m  ┃        ╚══════╝ ╚════╝  ╚════╝          ╚═╝    ╚════╝  ╚════╝ ╚══════╝      ┃ 
+\033[1;36m  ┃ @CopyRight : LộcTool                                                        ┃ 
+\033[1;37m  ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+\033[1;36m  ┃
+\033[1;37m  ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+\033[1;36m  ┃ Hôm Nay Là $vang$ngay Tháng $luc$thang Năm $hong$nam
+\033[1;37m  ┃ $loaitool
+\033[1;36m  ┃ Ip Máy Của Bạn :
+\033[1;37m  ┃ Facebook : fb.com/100054999610646
+\033[1;36m  ┃ Zalo : 0904755743
+\033[1;37m  ┃ Group Zalo : https://zalo.me/g/etdafm797
+\033[1;36m  ┃ ( Xin Anh EM Đừng Decode Tool Mình Ạ )
+\033[1;37m  ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+";
+
+
+
+@system('clear');
+echo $bannertoan;
+$a=date('d'); // 
+        $b=date("m");
+        $thoigian=date('d/m');
+        $a=date('d'); // 
+        $b=date("m");
+        $thoigian=date('d/m');
+$day = [];
+$day= date('d-m-y');
+$today = date("d-m-y");
+$d = date("d-m");
+$color2=array($a+$b+3740+$a*$b+49827);
+$cl1=$color2[array_rand($color2)];
+
+$color3=array($a+$b+3740+$a*$b+49827);
+$cl2=$color3[array_rand($color3)];
+
+$color4=array($a+$b+3740+$a*$b+49827);
+$cl3=$color4[array_rand($color4)];
+
+$color5=array($a+$b+3740+$a*$b+49827);
+$cl4=$color5[array_rand($color5)];
+$key = "$cl1"."$cl2"."$cl3"."$cl4";
+$long_url = urlencode("https://toolsever.000webhostapp.com/loc-tool/keytool.php?key=$key");
+$api_token = '623e499e0b6ebe20e3b27434b21b75ebceb31145';
+$api_url = "https://link1s.com/api?api={$api_token}&url={$long_url}";
+$result = @json_decode(file_get_contents($api_url), true);
+$checkkey = file_get_contents("https://toolsever.000webhostapp.com/loc-tool/keytool.php?key=$key");
+$d = date("d-m");
+$_check = file_exists("keylikelvb$d.txt");
+echo $BGreen."Truy Cập Vào Link : $BWhite".$result["shortenedUrl"]."".$BGreen." Để Lấy Key Miễn Phí\n";
+echo "\033[1;31m[\033[1;33mTHÔNG BÁO\033[1;31m]\033[1;32m Đây Là Tool Free. Key Tool Phải Lấy Lại Mỗi Ngày Nhé \n";
+if ($_check !='1'){
+$gh = date("d-m", strtotime("yesterday"));
+$yt = "keylikelvb$gh.txt";
+$_check2 = file_exists("$yt");
+if($_check2==1){
+  unlink("$yt");
+}
+for($j = 3;$j> 0;$j--){
+  echo $BCyan."Chuyển Hướng Đến Trang Lấy Key Sau $BCyan".$j;
+  sleep(1);
+  echo "\r";
+  echo "                                          \r";
+}
+system("termux-open-url ".$result["shortenedUrl"]."");
+echo $BWhite."- - - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
+    echo " \033[1;33m[\033[1;35mWARNING\033[1;33m] \033[1;32m Nhập Key Miễn Phí Đã Lấy : $BWhite";
+    $keyy = trim(fgets(STDIN));
+    }
+if ($_check =='1'){
+  $keyy = file_get_contents("keylikelvb$d.txt");
+  $vt = 1;
+  echo $BGreen."Đang Lấy Key...";
+  sleep(2);
+  echo "\r                                         \r";
+}
+if($keyy == ''){
+  echo $BRed."Key Sai Rồi, Vào Link Phía Trên Để Lấy Key\n";
+  exit;
+}
+  if($keyy == $key)
+      {
+             echo $BGreen."Key Hợp Lệ\n";
+             $mf = fopen("keylikelvb$d.txt", "w");
+             fwrite($mf, $keyy);
+             fclose($mf);
+      }
+      else
+      {
+           echo $BRed."Key Sai Rồi, Vào Link Phía Trên Để Lấy Key\n";
+           exit;
+      }
+@system('clear');
+echo $bannertoan;
+echo $vang." Tool theo thời gian sẽ mắc phải một số lỗi.
+Nếu có vấn đề gì vui lòng liên hệ admin\n";
+
+ 
+    echo "\033[1;37m\033[1;97m ┣━━━━\033[1;91m Trao Đổi Sub \033[1;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n";
+    echo "\033[1;37m ┃ ~\033[1;36m[\033[1;95m 1.1 \033[0m\033[1;36m]\033[1;36m Tool Trao Đổi Sub Đa Cookie           \033[1;37m┃\n"; 
+    echo "\033[1;37m\033[1;97m ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n"; 
+    echo "\n";       
+
+    echo "\033[1;37m\033[1;97m ┣━━━━\033[1;91m Tương Tác Chéo \033[1;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n";
+    echo "\033[1;37m ┃ ~\033[1;36m[\033[1;95m 2.1 \033[0m\033[1;36m] \033[1;36m Tool Tương Tác Chéo Đa Cookie        \033[1;37m┃\n";
+    echo "\033[1;37m ┃ ~\033[1;36m[\033[1;95m 2.2 \033[0m\033[1;36m] \033[1;36m Tool Tương Tác Chéo Đa Token         \033[1;37m┃\n";
+    echo "\033[1;37m\033[1;97m ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n"; 
+    echo "\n";    
+        echo "$red Lưu Ý: \033[1;33mPhải Nhập Đúng Lệnh Để Vào Tool
+    Ví Dụ : Tool Trao Đổi Sub Đa Cookie Min/Max => Gõ Là 1.1
+
+\n"; 
+       echo "\e[1;34m┌─[\e[1;37m\e[1;42mVui Lòng Chọn Chế Độ !! \e[0m\e[1;34m]
+└──╼ \e[1;35m❯\e[1;36m❯\e[1;31m❯ $yellow ";
+$nhaptool2 = trim(fgets(STDIN));
+echo "\033[1;94m Đang Vảo Tool, Vui Lòng Chờ !!\n";
+sleep(2);
+// Trao Đổi Sub
+if ($nhaptool2 == '1.1'){ 
+    if(!$sock = @fsockopen('www.google.com', 80)) { 
+        echo 'Vui lòng bật kết nối mạng'; exit(); } 
+        eval(file_get_contents('https://raw.githubusercontent.com/servertool/loctool/main/tdsck.php'));}
+// Tương Tác Chéo 
+else if ($nhaptool2 == '2.1'){ eval(file_get_contents('https://raw.githubusercontent.com/servertool/loctool/main/ttcck.php')); }
+else if ($nhaptool2 == '2.2'){ eval(file_get_contents('https://raw.githubusercontent.com/servertool/loctool/main/ttctk.php')); }
+?>
+            
